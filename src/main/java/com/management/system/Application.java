a4734c4b7701866dfbfbdb5f8199a20f860ad625
idnham <harshal.more@ideas.com>
2018-07-06 17:44:56 +0530

    Corrected the code to receive data from login page.

--- src/main/java/com/management/system/Application.java
+++ src/main/java/com/management/system/Application.java
@@ -8,8 +8,8 @@ public class Application  {
     
     public static void main(String[] args) {
     	System.setProperty("server.servlet.context-path", "/myapp");
+    	System.setProperty("server.port","8090");
         SpringApplication.run(Application.class, args);
-    }
-    
+    }  
     
 }
--- src/main/java/com/management/system/controller/MyRestController.java
+++ src/main/java/com/management/system/controller/MyRestController.java
@@ -1,6 +1,7 @@
 package com.management.system.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,11 +13,9 @@ import com.management.beans.LonginBean;
 public class MyRestController {
 	
 	 @RequestMapping(value = "/test", method=RequestMethod.POST,produces=MediaType.APPLICATION_JSON_VALUE)
-	    public @ResponseBody LonginBean getData() {
-	    	System.out.println(" I am here in test");	   
-	    	LonginBean bean = new LonginBean();
-	    	bean.setPassword("Testrs");
-	    	bean.setUsername("Testese");
-	        return bean;
+	    public @ResponseBody LonginBean getData(@RequestBody LonginBean loginBe) {
+		 	System.out.println(loginBe.getUsername());
+		 	System.out.println(loginBe.getPassword());	     
+	        return loginBe;
 	    }
 }
