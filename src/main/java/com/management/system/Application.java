af1fe9634e9047a197d5eca480400f32d7191d10
idnham <harshal.more@ideas.com>
2018-12-14 16:22:36 +0530

    Moved properties from Application.java to application.properties

--- src/main/java/com/management/system/Application.java
+++ src/main/java/com/management/system/Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application  {
     
     public static void main(String[] args) {
-    	System.setProperty("server.servlet.context-path", "/myapp");
-    	System.setProperty("server.port","8090");
+    /*	System.setProperty("server.servlet.context-path", "/myapp");
+    	System.setProperty("server.port","8090");*/
         SpringApplication.run(Application.class, args);
     }  
     
